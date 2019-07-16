b1acd6ce522789a5d572f25546ecad7a2c27d871
Filipp Vynnykov <ynov07@rambler.ru>
2019-07-16 17:42:19 +0300

    Add project files.

---
+++ LogAn.Test/LogAnTest.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+
+namespace LogAn.Test
+{
+    [TestFixture]
+    public class LogAnTest
+    {
+        [TestCase("twst.SLF", true)]
+        [TestCase("twst.slf", true)]
+        [Category("1")]
+        public void IsValidFileName_CorrectName_ReturnTrue(string filename, bool expectedResult)
+        {
+            LogAnalyzer analyzer = new LogAnalyzer();
+            bool result = analyzer.IsValidLogFileName(filename);
+            result.Should().Be(expectedResult);
+        }
+
+        [Test]
+        [Category("2")]
+        public void IsValidFileName_IncorrectName_ThrowException()
+        {
+            LogAnalyzer analyzer = new LogAnalyzer();
+            Action act =() => analyzer.IsValidLogFileName("filename");
+            act.Should().Throw<ArgumentException>().WithMessage(LogAnalyzer.WRONG);
+        }
+
+        [Test]
+        public void IsValidFileName_IncorrectName_ThrowException2()
+        {
+            LogAnalyzer analyzer = new LogAnalyzer();
+            var ex = Assert.Catch<ArgumentException>(()=> analyzer.IsValidLogFileName("filename"));
+            StringAssert.Contains(LogAnalyzer.WRONG, ex.Message);
+        }
+    }
+}
---
+++ LogAn/LogAnalyzer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogAn
+{
+    public class LogAnalyzer
+    {
+        public const string WRONG = "Wrong extension";
+
+        public bool IsValidLogFileName(string filName)
+        {
+            if (!filName.EndsWith(".SLF", StringComparison.InvariantCultureIgnoreCase))
+            {
+                throw new ArgumentException(WRONG);
+            }
+            return true;
+        }
+    }
+}
